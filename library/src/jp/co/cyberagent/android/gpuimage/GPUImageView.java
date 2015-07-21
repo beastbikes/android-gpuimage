cfa6e4bf9bf22fc3f7fa39cfd1f574fef58c2314
lizhenghlh <18368053@qq.com>
2015-07-21 15:00:39 +0800

    fix bug

--- library/src/jp/co/cyberagent/android/gpuimage/GPUImageView.java
+++ library/src/jp/co/cyberagent/android/gpuimage/GPUImageView.java
@@ -147,4 +147,8 @@ public class GPUImageView extends GLSurfaceView {
     	this.mGPUImage.rotate(degree);
 	}
     
+    public void deleteImage(){
+    	this.mGPUImage.deleteImage();
+    }
+    
 }
